5f73fdedb41ee20fa011886c9cafa812905a91a0
Lionel Desorme <lionel.desorme@free.fr>
2017-10-13 18:05:23 +0200

    Disp

--- DispForm/Form1.cs
+++ DispForm/Form1.cs
@@ -35,9 +35,6 @@ namespace DispForm
 
             _g.DrawLine(Pens.Black, new Point(Width / 2, 0), new Point(Width / 2, Height));
             _g.DrawLine(Pens.Black, new Point(0, Height / 2), new Point(Width, Height / 2));
-
-            ShowPoint(-1.0, 0.0);
-            ShowPoint(+1.0, 0.0);
         }
 
         private Point GetPoint(double x, double y)
@@ -45,26 +42,69 @@ namespace DispForm
             return new Point((int)(Width / 2 + _mx * x), (int)(Height / 2 + _my * y));
         }
 
-        private void ShowPoint(double x, double y)
+        private void ShowPoint(P2D M)
         {
-            var p = GetPoint(x, y);
+            var p = GetPoint(M.x, M.y);
             Rectangle rectangle = new Rectangle(p.X - L, p.Y - L, 2*L, 2*L);
-            _g.DrawEllipse(Pens.Red, rectangle);
+            if (M.g!=null)
+                _g.DrawEllipse(Pens.Red, rectangle);
+            else
+                _g.DrawEllipse(Pens.Green, rectangle);
+        }
+
+        private void ShowLine(P2D A, P2D B, Pen p)
+        {
+            var pA = GetPoint(A.x, A.y);
+            var pB = GetPoint(B.x, B.y);
+            _g.DrawLine(p, pA, pB);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             ShowSetUp();
+            StartAlgo();
         }
 
         public void Display(P2D p)
         {
-          
+            ShowPoint(p);
+            if (p.g != null)
+            {
+                ShowPoint(p.g.A);
+                ShowPoint(p.g.B);
+
+                ShowLine(p, p.g.A, Pens.Blue);
+                ShowLine(p, p.g.B, Pens.Blue);
+                ShowLine(p.g.A, p.g.B, Pens.Yellow);         
+            }
         }
 
         public void Display(D2 d)
         {
-          
+            ShowPoint(d.A);
+            ShowPoint(d.B);
+        }
+
+        public void StartAlgo()
+        {
+            var mp = new MyPoints();
+            mp.GenDistances2();
+            Console.WriteLine(mp);
+            mp.CheckPointMatch(this);
+
+
+            mp.GenPoints();
+            mp.GenDistances2();
+            Console.WriteLine(mp);
+            mp.CheckPointMatch(this);
+
+            mp.GenPoints();
+            mp.GenDistances2();
+            Console.WriteLine(mp);
+            mp.CheckPointMatch(this);
+
+            mp.GenPoints();
+            mp.CheckPointMatch(this);
         }
     }
 }
